848d1d8745de7288f3c4216484ccd0e5a107bce5
Shota Matsubara <baramatsubonzo@gmail.com>
2025-09-10 11:33:09 +1000

    Seaquential matrix

--- main.cpp
+++ main.cpp
@@ -1,7 +1,45 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <chrono>
+
+double **A, **B, **C;
 
 int main(int argc, char* argv[])
 {
-    printf("Hello\n");
+    long long N = 1500;
+
+    A = new double*[N];
+    B = new double*[N];
+    C = new double*[N];
+
+    srand(42);
+
+    for (int i = 0; i < N; i++)
+    {
+        A[i] = new double[N];
+        B[i] = new double[N];
+        C[i] = new double[N];
+        for (int j=0; j<N; j++)
+        {
+            A[i][j] = rand();
+            B[i][j] = rand();
+        }
+    }
+
+    auto start_time = std::chrono::high_resolution_clock::now();
+
+    for (int i=0; i<N; i++)
+        for (int j=0; j<N; j++)
+        {
+            double total = 0;
+            for (int k = 0; k < N; k++)
+                total += A[i][k] *B[k][j];
+            C[i][j] = total;
+        }
+    auto end_time = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> duration = end_time - start_time;
+    printf("%f seconds\n", duration.count());
+
     return 0;
 }
